8431e137007caef7dd7c438c0de540cbe819dee3
Prawirdani <prawirdani@gmail.com>
2024-05-31 15:38:30 +0700

    update: add icon on 404

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,33 @@ import { Toaster } from './components/ui/toaster';
 import { dashboardRoutes } from './pages/dashboard';
 import { publicRoutes } from './pages';
 import { H1 } from '@/components/typography';
+import { Frown } from 'lucide-react';
 
 export default function App() {
-	const router = createBrowserRouter([
-		...publicRoutes,
-		...dashboardRoutes,
-		{
-			path: '*',
-			element: <NotFound />,
-		},
-	]);
-	return (
-		<AuthProvider>
-			<RouterProvider router={router} />
-			<Toaster />
-		</AuthProvider>
-	);
+  const router = createBrowserRouter([
+    ...publicRoutes,
+    ...dashboardRoutes,
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ]);
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AuthProvider>
+  );
 }
 function NotFound() {
-	return (
-		<div className="h-screen">
-			<div className="h-full flex place-items-center">
-				<H1 className="mx-auto text-primary">Page Not Found</H1>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-screen">
+      <div className="h-full flex flex-col place-items-center">
+        <div className="my-auto [&>*]:mx-auto [&>*]:text-primary">
+          <Frown size={64} />
+          <H1 className="text-primary">Page Not Found!</H1>
+        </div>
+      </div>
+    </div>
+  );
 }
